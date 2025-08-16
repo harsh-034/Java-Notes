04bf7e48d4cdbe116f9ea2d78d120ff30081f51d
harsh-034 <harshgupta00034@gmail.com>
2025-08-16 15:47:01 +0530

    update

---
+++ Function/SumOfTowNumber.java
@@ -0,0 +1,55 @@
+// Sum Of Two number using Function
+
+
+import java.util.*;
+
+public class SumOfTowNumber {
+    public static int SumPrint(int a, int b){
+        int sum = a + b ;
+
+        System.out.print("Total number of sum : "+sum);
+        return 1;  // public staitic are use of "int" then i am using " return 1;"
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number :");
+        int a = sc.nextInt();
+        System.out.println("Enter the Scound Number :");
+        int b = sc.nextInt();
+        SumPrint(a,b);
+
+    }
+}
+
+
+//Second Method 
+
+/* 
+import java.util.*;
+
+public class SumOfTowNumber {
+    public static int SumPrint(int a, int b){
+        int sum = a + b ;
+        return sum;  // retrn kar denge niche
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number :");
+        int a = sc.nextInt();
+        System.out.println("Enter the Scound Number :");
+        int b = sc.nextInt();
+        int sum = SumPrint(a,b); //ther are function call and after store sum virable and go to print 
+        // sum ther are get return value " return sum"
+         System.out.print("Total number of sum : "+sum);
+
+    }
+}
+
+*/
+
--- Loop_base_qution/InvertedhelfePareamend.java
+++ Loop_base_qution/InvertedhelfePareamend.java
@@ -18,15 +18,15 @@ public class InvertedhelfePareamend {
     public static void main(String[] args) {
         
     
-    //  int n = 4;
-    //     for(int i=n ; i >= 1; i--)
-    //     {
-    //         for(int j=i; j>= 1; j--)
-    //         {
-    //             System.out.print("*");
-    //         }
-    //         System.out.println(" ");
-    //     }
+     int n = 4;
+        for(int i=n ; i >= 1; i--)
+        {
+            for(int j=i; j>= 1; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
 
 
 
