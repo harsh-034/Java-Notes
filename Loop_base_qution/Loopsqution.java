1d6020eedff1be2df1c3971edf700fde2de878b8
Harsh Gupta <166977482+harsh-034@users.noreply.github.com>
2025-08-16 15:32:19 +0530

    Loop qution

---
+++ Loop_base_qution/Loopsqution.java
@@ -0,0 +1,26 @@
+//print the patern  Rectangel
+
+/* 
+
+*****
+*****
+*****
+*****
+
+*/
+
+
+
+public class Loopsqution {
+    public static void main(String[] args) {
+       for(int i=1;i<=4;i++)
+       {
+        for(int j=1;j<=5;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println(" ");
+
+       }
+    }
+}
---
+++ Loop_base_qution/Space_and_ster.java
@@ -0,0 +1,34 @@
+/* 
+  
+  
+   *
+  **
+ ***
+****    
+ 
+*/
+ 
+
+
+
+
+
+public class Space_and_ster {
+   public static void main(String[] args) {
+    int n=4;
+
+    for(int i=1; i<=n; i++)//1
+    {
+      for(int j=1; j<=n-i;j++)//4-1
+      {
+        System.out.print(" ");// print 3 bar print 
+      }
+      for(int j=1; j<=i;j++)//i=1 then print 1 time * j ke bad continoue karega 
+      {
+        System.out.print("*");
+      }
+      System.out.println("");//new line 
+    }
+   }
+    
+}
