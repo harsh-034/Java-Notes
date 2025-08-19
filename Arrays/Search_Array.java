4782724d824afcef006fff3dc402a8b7ece374ff
harsh-034 <harshgupta00034@gmail.com>
2025-08-19 22:05:14 +0530

    add Search arry like lenear Qs:

--- Arrays/Search_Array.java
+++ Arrays/Search_Array.java
@@ -1,5 +1,34 @@
-package Arrays;
+//Qs :Take anarray as input from the user. Search for a given number X and print the index number at which it occurs.
+//
 
+
+// this is liner search
+
+package Arrays;
+import java.util.*;
 public class Search_Array {
-    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a size for array :");
+        int size = sc.nextInt();
+
+        int[] num = new int[size];
+
+        for(int i=0; i<size; i++)
+        {
+            num[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the search number: ");
+        int Search = sc.nextInt();
+
+        for(int i=0; i<size ; i++)
+        {
+            if(Search == num[i]){
+               
+                System.out.println(Search+" are index number  :"+i);
+            }
+        }
+    }
 }
