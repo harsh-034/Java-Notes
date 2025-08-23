9c722a700cc843fa5339d3c425da529f335828dd
harsh-034 <harshgupta00034@gmail.com>
2025-08-23 11:51:48 +0530

    Complete the String Builder

---
+++ String/String_Builder.java
@@ -0,0 +1,59 @@
+package String;
+
+public class String_Builder {
+
+    public static void main(String[] args){
+        //String in Java are immutable becouse i will not cange after one time decration    
+
+        StringBuilder name = new StringBuilder("Harsh");
+        System.out.println(name);  //output harsh
+
+        // charAt( index number )   this is index number are get and return value of thish number 
+
+        //Example
+         System.out.println(name.charAt(3));// output: s
+
+
+         //setcharAt(indexnumber, replace value )  ther use for replace value on index number 
+
+         // example 
+         StringBuilder S_name = new  StringBuilder("Gupta");
+
+         S_name.setCharAt(0,'H'); //index no 0 (G) replace to H
+         System.out.println(S_name); // Hupta
+
+         // insert(index no, add string ) insert function are use for add index number new string 
+
+         // example
+         S_name.insert(0,"G");
+         System.out.println(S_name);  // output : GHupta 
+
+         S_name.insert(3,"p");
+         System.out.println(S_name);// output : GHuppta
+
+         // Delete(index number are strting , index num are end)
+         
+         //Delete extra p
+         S_name.delete(3,4); // extra p are deleted
+         System.out.println(S_name);
+
+         //append(string add stake formet )
+         // 
+         StringBuilder str = new StringBuilder("H");
+         str.append("e");  // ther are string use then str + "e"  its lendie and consume lot time 
+         str.append("l");  
+         str.append("l");
+         str.append("o");
+         System.out.println(str); //output: hello 
+         
+         //length
+
+         System.out.println(str.length());
+
+         //reverse are use for ulta
+          str.reverse();
+          System.out.println(str);// reverse are output :  olleh
+
+
+    }
+}
