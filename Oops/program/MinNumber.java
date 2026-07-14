9c8beae3fe7ee4140d7f98991df83c5491a85dfa
harsh-034 <harshgupta00034@gmail.com>
2026-07-15 02:23:37 +0530

    find minimumm number in Array

---
+++ Oops/program/MinNumber.java
@@ -0,0 +1,14 @@
+package Oops.program;
+
+public class MinNumber {
+    int minimumNumber(int[] arr){
+        int min = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+}
--- Oops/program/main.java
+++ Oops/program/main.java
@@ -4,7 +4,15 @@ public class main {
     public static void main(String[] args){
         int [] arr = {3,22,44,11};
         Mexnumber m = new Mexnumber();
+        MinNumber n = new MinNumber();
+        //-------------------------------------------------------
+        //find meximumm number use return
          int result = m.meximumNumber(arr);
          System.out.println("this is your mex value : "+ result);
+         //---------------------------------------------------
+        //find minumm number of arr
+        int result1 = n.minimumNumber(arr);
+        System.out.println("this is your minimum Value : "+result1);
+
     }
 }
